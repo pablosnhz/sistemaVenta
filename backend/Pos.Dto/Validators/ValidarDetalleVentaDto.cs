3fa0bff52fc3f217cd518eb2270bff4f1f4312bd
PabloSnhz <pablosanhuezaaa@gmail.com>
2025-07-02 19:45:23 -0300

    fix error tracking files, and now solution updated venta/ventadetalle validators

---
+++ backend/Pos.Dto/Validators/ValidarDetalleVentaDto.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Pos.Dto.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.Dto.Validators
+{
+    public class ValidarDetalleVentaDto : AbstractValidator<DetalleVentaDto>
+    {
+        public ValidarDetalleVentaDto()
+        {
+            RuleFor(dv => dv.idVenta)
+                .GreaterThan(0).WithMessage("No se ha especificado el ID de la venta.");
+
+            RuleFor(dv => dv.idProducto)
+                .GreaterThan(0).WithMessage("No se ha especificado el ID del producto.");
+
+            RuleFor(dv => dv.nombreProducto)
+                .NotEmpty().WithMessage("Es necesario especificar el nombre del producto.")
+                .MaximumLength(50).WithMessage("El nombre del producto no debe de superar los 50 caracteres.");
+
+            RuleFor(dv => dv.precio)
+                .NotEmpty().WithMessage("Es necesario especificar el precio del producto")
+                .GreaterThan(0).WithMessage("El precio debe ser mayor a cero.");
+
+            RuleFor(dv => dv.cantidad)
+                .NotEmpty().WithMessage("Es necesario especificar la cantidad de venta.")
+                .GreaterThan(0).WithMessage("La cantidad de venta debe ser mayor a cero.");
+
+            RuleFor(dv => dv.descuento)
+                .GreaterThanOrEqualTo(0).WithMessage("El descuento no puede ser menor a cero.");
+
+            RuleFor(dv => dv.total)
+                .GreaterThan(0).WithMessage("El total debe ser mayor a cero.");
+
+        }
+    }
+}
---
+++ backend/Pos.Dto/Validators/ValidarVentaDto.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Pos.Dto.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.Dto.Validators
+{
+    internal class ValidarVentaDto : AbstractValidator<VentaDto>
+    {
+        public ValidarVentaDto()
+        {
+            RuleFor(v => v.dni)
+                .NotEmpty().WithMessage("Es necesario especificar el numero de identificacion del cliente.")
+                .MaximumLength(20).WithMessage("El numero de identificacion no de superar los 20 caracteres.");
+
+            RuleFor(v => v.cliente)
+                .NotEmpty().WithMessage("Se debe especificar el nombre del cliente")
+                .MinimumLength(3).WithMessage("El nombre del cliente debe tener por lo menos 3 caracteres")
+                .MaximumLength(50).WithMessage("El nombre del cliente no debe de superar los 50 caracteres.");
+
+            RuleFor(v => v.descuento)
+                .GreaterThanOrEqualTo(0).WithMessage("El descuento de la venta no puede ser mejor que cero.");
+
+            RuleFor(v => v.total)
+                .GreaterThan(0).WithMessage("El total de la venta debe ser mayor de cero.");
+
+            RuleFor(v => v.idUsuario)
+                .GreaterThan(0).WithMessage("No se ha especificado el usuario que realizo la venta.");
+
+            RuleFor(v => v.estado)
+                .IsInEnum().WithMessage("El estado de la venta no es valido, selecciona una opcion de la lista desplegable.");
+
+            RuleFor(v => v.fechaAnulada)
+                .Null().When(v => v.estado == Model.Model.EstadoVenta.Activa).WithMessage("La fecha de anulacion debe ser nula si el estado de la venta es: 'Activa'.")
+                .NotEmpty().When(v => v.estado == Model.Model.EstadoVenta.Anulada).WithMessage("La fecha de anulacion es requerida si el estado de la venta es anulada.")
+                .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Now)).When(v => v.fechaAnulada.HasValue).WithMessage("La fecha de anulacion no puede ser mayor a la fecha actual.");
+
+            RuleFor(v => v.motivo)
+                .NotEmpty().When(v => v.estado == Model.Model.EstadoVenta.Anulada).WithMessage("El motivo de anulacion de la venta es requerido cuando la venta es: 'Anulada'");
+
+            RuleFor(v => v.usuarioAnula)
+                .NotEmpty().When(v => v.estado == Model.Model.EstadoVenta.Anulada).WithMessage("El usuario que anula la venta es requerido si la venta es: 'Anulada'.");
+        }
+    }
+}
