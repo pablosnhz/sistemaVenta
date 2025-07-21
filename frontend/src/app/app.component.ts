4bc6643344ed506c6cfa5a3986011b0f9169d68d
PabloSnhz <pablosanhuezaaa@gmail.com>
2025-07-21 18:59:03 -0300

    Sidebar design styles

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaRolComponent } from './pages/roles/lista-rol/lista-rol.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pages/roles',
+    component: ListaRolComponent,
+  },
+];
